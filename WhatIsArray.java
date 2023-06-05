c28b8245bd9e133748237193fae2d84dd6b85ff3
SaitovN <nuriksaitov03@gmailcom>
2023-06-05 16:56:25 +0600

    Create WhatIsArray.java

---
+++ WhatIsArray.java
@@ -0,0 +1,13 @@
+package academy.devonline.java.basic.section01_setup.section06_setup;
+
+public class WhatIsArray {
+    public static void main(String[] args) {
+        var a = 1;
+        var b = 2;
+        var c = 3;
+        var e = 4;
+        var t = 5;
+        int[]array = {1,2,3,4,5};
+    }
+}
+
